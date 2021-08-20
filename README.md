35da7e1301afdd6e8aeff2a3d5e0057bb5c5f611
JinaKim77 <G00353420@gmit.ie>
2021-08-20 20:27:29 +0100

    Finished this project - Updated queries

--- README.md
+++ README.md
@@ -2,15 +2,22 @@
 
 <br>
 
-Investigate a poorly designed database for Udiddit, a social news aggregator.
+Databases need to be structured properly to enable efficient and effective querying and analysis of data. 
+<br>
+Build normalized, consistent, and performant relational data models. 
+<br>
 
-You’ll design a new, normalized and performant database and migrate over data from the previous database.
+Use SQL Database Definition Language (DDL) to create the data schemas designed in Postgres and apply SQL Database Manipulation Language (DML) to migrate data from a denormalized schema to a normalized one. 
+
+* Investigate a poorly designed database for Udiddit, a social news aggregator.
+
+* Design a new, normalized and performant database and migrate over data from the previous database.
 
 <br>
 
 ## Instructions
 
-All your tasks are outlined in [Here](https://github.com/JinaKim77/PostgreSQL-project/blob/main/udiddit-a-social-news-aggregator.pdf)
+All your tasks are outlined in [Here](https://github.com/JinaKim77/PostgreSQL-project/blob/main/Udiddit, a social news aggregator.pdf)
 
 You need to complete the following in the template:
 
--- Used_DDL_DQL.sql
+++ Used_DDL_DQL.sql
@@ -3,41 +3,43 @@
 # PART 2. Create the DDL for new schema
 
 CREATE TABLE users(
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR(25) NOT NULL,
-    "logged_in" DATE
+  "id" SERIAL PRIMARY KEY,
+  "username" VARCHAR(25) NOT NULL,
+  "logged_in" DATE
 );
 
 CREATE UNIQUE INDEX ON users (LOWER("username"));
 
 CREATE TABLE topics (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR(30) UNIQUE NOT NULL,
-    "description" VARCHAR(500)
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(30) UNIQUE NOT NULL,
+  "description" VARCHAR(500)
 );
 
 CREATE TABLE comments (
-    "id" SERIAL PRIMARY KEY,
-    "text_content" TEXT NOT NULL,
-    "user_id" INTEGER REFERENCES users ON DELETE SET NULL
+  "id" SERIAL PRIMARY KEY,
+  "text_content" TEXT NOT NULL,
+  "user_id" INTEGER NOT NULL REFERENCES users ON DELETE SET NULL
 );
+
+CREATE INDEX ON comments ("id");
 
 CREATE TABLE posts (
-    "id" SERIAL PRIMARY KEY,
-    "tile" VARCHAR(100) NOT NULL,
-    "url" VARCHAR (1000) DEFAULT NULL,
-    "text_content" TEXT DEFAULT NULL,
-    "topic_id" INTEGER REFERENCES topics ON DELETE CASCADE,
-    "user_id" INTEGER REFERENCES users ON DELETE SET NULL,
-    "commnet_id" INTEGER REFERENCES comments ON DELETE CASCADE,
-    "upvotes" TEXT,
-    "downvotes" TEXT
+  "id" SERIAL PRIMARY KEY,
+  "tile" VARCHAR(100) NOT NULL,
+  "url" VARCHAR (1000) DEFAULT NULL,
+  "text_content" TEXT DEFAULT NULL,
+  "topic_id" INTEGER NOT NULL REFERENCES topics ON DELETE CASCADE,
+  "user_id" INTEGER NOT NULL REFERENCES users ON DELETE SET NULL,
+  "commnet_id" INTEGER REFERENCES comments ON DELETE CASCADE
 );
 
-CREATE TABLE user_post_like (
-    "user_id" INTEGER REFERENCES users ON DELETE SET NULL,
-    "post_id" INTEGER REFERENCES posts ON DELETE CASCADE,
-    PRIMARY KEY("user_id","post_id")
+CREATE INDEX ON posts ("url");
+
+CREATE TABLE votes (
+  "user_id" INTEGER REFERENCES users ON DELETE SET NULL,
+  "post_id" INTEGER REFERENCES posts ON DELETE CASCADE,
+  "votes" TEXT
 );
 
 
@@ -45,17 +47,36 @@ CREATE TABLE user_post_like (
 
 ALTER TABLE topics ALTER COLUMN "description" SET DEFAULT NULL;
 
+INSERT INTO users ("username")
+  SELECT DISTINCT username FROM bad_comments;
 
-INSERT INTO users ("username") SELECT DISTINCT username FROM bad_comments;
+INSERT INTO comments ("text_content","user_id")
+  SELECT DISTINCT b.text_content, u.id
+  FROM bad_comments AS b
+  JOIN users AS u ON b.username=u.username
+  WHERE b.text_content IS NOT NULL AND b.username IS NOT NULL;
 
+INSERT INTO topics ("name")
+  SELECT DISTINCT topic FROM bad_posts;
 
-INSERT INTO comments ("text_content","user_id") SELECT DISTINCT b.text_content, u.id
-                                                FROM bad_comments AS b JOIN users AS u ON b.username=u.username
-                                                WHERE b.text_content IS NOT NULL AND b.username IS NOT NULL;
+INSERT INTO posts ("tile","url","text_content","topic_id","user_id")
+  SELECT b.title, b.url, b.text_content, t.id, u.id
+  FROM bad_posts AS b
+  JOIN topics AS t ON b.topic=t.name
+  JOIN users AS u ON u.username=b.username;
 
+INSERT INTO votes ("user_id","post_id","votes")
+  SELECT DISTINCT u.id, p.id, REGEXP_SPLIT_TO_TABLE(b.upvotes,',') AS votes
+  FROM bad_posts AS b
+  JOIN users AS u
+  ON b.username=u.username
+  JOIN posts AS p
+  ON u.id=p.user_id;
 
-INSERT INTO topics ("name") SELECT DISTINCT topic FROM bad_posts;
-
-INSERT INTO posts ("tile","url","text_content","topic_id","user_id", "upvotes","downvotes")
-      SELECT b.title, b.url, b.text_content, t.id, u.id, b.upvotes,b.downvotes
-      FROM bad_posts AS b JOIN topics AS t ON b.topic=t.name JOIN users AS u ON u.username=b.username;
+INSERT INTO votes ("votes")
+  SELECT DISTINCT REGEXP_SPLIT_TO_TABLE(b.downvotes,',') AS votes
+  FROM bad_posts AS b
+  JOIN users AS u
+  ON b.username=u.username
+  JOIN posts AS p
+  ON u.id=p.user_id;
